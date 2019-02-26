8e5a98bd47bd44302d897ad1f3d3366d6f23bf4e
AlexanderKruk <44405836+AlexanderKruk@users.noreply.github.com>
2019-02-26 10:06:01 +0300

    Add icons, but it not centered with text

--- src/InfoPeople.js
+++ src/InfoPeople.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { FaWeight } from 'react-icons/fa';
+import { MdPerson, MdPublish } from 'react-icons/md';
+import { IconContext } from "react-icons";
+import './InfoList.css';
 
 const InfoPeople = ({name, height, mass}) => {
 	return (
-		<div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 f4 lh-copy measure'>
-			<p>Name:<strong> {name} </strong></p>
-			<p>Height:<strong>{height}cm</strong></p>
-			<p>Weight: <strong>{mass}kg</strong></p>
+		<div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 f4 lh-copy measure '>
+		<span className='vcentered'>
+		 <IconContext.Provider value={{ color: "blue", size: 32, verticalAlign: 'middle', className: "global-class-name" }}>
+					<MdPerson />
+		  </IconContext.Provider>
+            <strong> {name} </strong></span>
+			<p><MdPublish /><strong>{height}cm</strong></p>
+			<p><FaWeight /><strong>{mass}kg</strong></p>
 		</div>
 		);
 }
