b4da70bd95d6010ea9c33ebaffeeec3d60b11666
AlexanderKruk <44405836+AlexanderKruk@users.noreply.github.com>
2019-02-26 11:01:03 +0300

    Updates

---
+++ precache-manifest.29e626895f59922244e8bcedbce5f68f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb274bfe9c62d2353308",
+    "url": "/AlexanderKruk/useapi.git/static/js/runtime~main.eb274bfe.js"
+  },
+  {
+    "revision": "28f1edadc58956adebce",
+    "url": "/AlexanderKruk/useapi.git/static/js/main.28f1edad.chunk.js"
+  },
+  {
+    "revision": "5a87c5a89707a63c9a19",
+    "url": "/AlexanderKruk/useapi.git/static/js/2.5a87c5a8.chunk.js"
+  },
+  {
+    "revision": "28f1edadc58956adebce",
+    "url": "/AlexanderKruk/useapi.git/static/css/main.6542ec69.chunk.css"
+  },
+  {
+    "revision": "5a87c5a89707a63c9a19",
+    "url": "/AlexanderKruk/useapi.git/static/css/2.0331a278.chunk.css"
+  },
+  {
+    "revision": "ee0c7cf502d7532e2b55042e07b0965e",
+    "url": "/AlexanderKruk/useapi.git/index.html"
+  }
+];
